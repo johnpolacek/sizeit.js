4da79778668d38cfbd0c3bd79c3ee3192328faf6
John Polacek <johnpolacek@gmail.com>
2011-06-20 09:12:34 -0500

    minor jslint validation fixes

--- sizeit.js
+++ sizeit.js
@@ -1,74 +1,64 @@
 (function()
 {
 	var config = {};
-	var screenW = windowSize();
+	var screenW;
 	
-	function windowSize()
-	{
-		// Internet Explorer
-		if (document.body && document.body.offsetWidth)
-		{
-			return document.body.offsetWidth;
+	function windowSize() {
+		var wSize;
+        // Internet Explorer
+		if (document.body && document.body.offsetWidth) {
+			wSize = document.body.offsetWidth;
 		}
 		// Internet Explorer
-		else if (document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.offsetWidth)
-		{
-			return document.documentElement.offsetWidth;
+		else if (document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.offsetWidth) {
+			wSize = document.documentElement.offsetWidth;
 		}
 		// All other browsers
-		else if (window.innerWidth)
-		{
-			return window.innerWidth;
+		else if (window.innerWidth) {
+			wSize = window.innerWidth;
 		}
+        
+        return wSize;
 	}
 	
-	function update()
-	{
+	function update() {
 		screenW = windowSize();
         var newSize;
+        var i;
 		
-		for (var i = 0; i < config.settings.length; i++)
-		{
+		for (i = 0; i < config.settings.length; i++) {
 			var setting = config.settings[i];
 			
-			if (screenW < setting.max)
-			{
+			if (screenW < setting.max) {
 				newSize = setting;
 				break;
             }
-			else if (!setting.max)
-			{
+			else if (!setting.max) {
 				newSize = setting;
             }
         }
 		
-		if (!config.size || newSize.name != config.size.name)
-		{
+		if (!config.size || newSize.name != config.size.name) {
             config.size = newSize;
-        }
-		else
-		{
+        } else {
             newSize = null;
         }
         
-        if (config.size.css && newSize)
-		{
+        if (config.size.css && newSize) {
+            
             // If there is a stylesheet, update it, otherwise make new
-            if (config.css)
-			{
+            if (config.css) {
 				var links = document.getElementsByTagName('link');
 				
-				for (var i = 0; i < links.length; i++)
-				{
-					if (links[i].getAttribute('title') == 'sizeit')
-					{
+				for (i = 0; i < links.length; i++) {
+					
+                    if (links[i].getAttribute('title') == 'sizeit') {
 						config.css.href = config.size.css;
                         break;
                     }
                 }
 			}
-			else
-			{
+			else {
 				config.css = document.createElement('link');
                 config.css.href = config.size.css;
                 config.css.type = 'text/css';
@@ -85,12 +75,11 @@
             config.settings = arguments;
             
             // sort by breakpoints, lowest to highest
-            for (var i = 0; i < config.settings.length; i++)
-            {
-                for (var j = i + 1; j < config.settings.length; j++)
-                {
-                    if (config.settings[i].max > config.settings[j].max)
-                    {
+            for (var i = 0; i < config.settings.length; i++) {
+                
+                for (var j = i + 1; j < config.settings.length; j++) {
+                    
+                    if (config.settings[i].max > config.settings[j].max) {
                         s = config.settings[j];	
                         config.settings[j] = config.settings[i];
                         config.settings[i] = s;
@@ -100,30 +89,25 @@
             
             update();
             
-            if (window.addEventListener)
-			{
+            if (window.addEventListener) {
                 window.addEventListener('resize', update, false);
             }
-			else if (window.attachEvent)
-			{
+			else if (window.attachEvent) {
                 window.attachEvent('resize', update);
             }
-			else
-			{
+			else {
                 window.onresize = update;
             }
             
         },
         
-        size : function()
-		{
+        size : function() {
             return config.size.name;
         },
         
-        width : function()
-		{
-            return windowSize();
+        width : function() {
+            return screenW;
         }
     };
 	
-})();
+}());
